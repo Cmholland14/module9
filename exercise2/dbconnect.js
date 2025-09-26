05f83f23e8465f1c156cf88bf98ec81f9e007748
Cholland14 <cmholland14@gmail.com>
2025-09-26 14:02:05 +1200

    exercise3

--- exercise2/dbconnect.js
+++ exercise2/dbconnect.js
@@ -1,18 +1,31 @@
-'use strict';
-const Mongoose = require('mongoose');
-// if the connection fails, try 127.0.0.1 instead of localhost below
-const uri = process.env.DB_URI ||
-"mongodb://localhost/myFirstDatabase";
+"use strict";
+const { Sequelize } = require("sequelize");
 
-// Connect to MongoDB
-Mongoose.connect(uri)
-.then(() => console.log('MongoDB Connected'))
-.catch(error => console.log('MongoDB Error: ' + error.message));
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
-// Get the default connection
-const db = Mongoose.connection;
+const connectMysql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      `Successful connection to MySQL Database ${process.env.DB_NAME}`
+    );
+  } catch (error) {
+    console.error("Unable to connect to MySQL database:", error);
+    process.exit(1);
+  }
+};
 
-// Bind connection to error event (to get notification of connection errors)
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+connectMysql();
 
-exports.Mongoose = Mongoose;
+module.exports = {
+  Sequelize: sequelize,
+  connectMysql,
+};
